3d22e108a186e7b948bac4afb23505dd6ad4b1c9
Weipeng <weipeng.zhang@17zuoye.com>
2020-10-18 21:37:41 +0800

    login update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,20 @@
         <img src="../assets/logo.png" alt="">
       </div>
       <!-- 登录表单区域 -->
-      <el-form label-width="0px" class='login_form'>
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class='login_form'>
         <!-- 用户名 -->
-        <el-form-item>
-          <el-input prefix-icon="el-icon-user-solid"></el-input>
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="el-icon-user"></el-input>
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item>
-          <el-input prefix-icon="el-icon-edit-outline"></el-input>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" prefix-icon="el-icon-key"
+          type="password"></el-input>
         </el-form-item>
         <!-- 按钮 -->
         <el-form-item class='btns'>
-          <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -26,7 +27,43 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      // 这是登录表单的数据绑定对象
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      // 这是表单的验证规则对象
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 5, max: 15, message: '长度在 5 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetLoginForm () {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return null
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (!res || !res.success) return this.$message.error('登录失败')
+        this.$message.success('登录成功')
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
+      })
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.prototype.$message = Message // 每个组件都可以通过this.$message访问弹窗组件
